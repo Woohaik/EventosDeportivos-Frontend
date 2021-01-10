dfcbaea4ebcdc42ac8640d5f3087701d9926663c
Woohaik <wilfredo.hernandez.ar@gmail.com>
2021-01-10 16:55:05 +0100

    Correcion de filtro de eventos

--- src/presentation/components/events/CardBlock.tsx
+++ src/presentation/components/events/CardBlock.tsx
@@ -8,13 +8,14 @@ import { useSelector } from "react-redux";
 
 
 const CardBlock: FC = () => {
-    const allTheEvents = useSelector((state: RootState) => state.event.filteredEvents);
+    const allTheEvents = useSelector((state: RootState) => state.event.events);
+    const allFilteredEvents = useSelector((state: RootState) => state.event.filteredEvents);
 
 
 
     return (
         <Fragment>
-            { allTheEvents.length === 0 ? (<h2>Aun No Hay eventos</h2>) : allTheEvents.map(cardEvent =>
+            { allTheEvents.length === 0 ? (<h2>Aun No Hay eventos</h2>) : allFilteredEvents.map(cardEvent =>
             (
                 <div key={cardEvent.id} className="col-md-6 col-lg-4 col-sm-12 my-3">
                     <EventCard event={cardEvent} />
